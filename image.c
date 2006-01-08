79018e1b03c7bf88bc7df554fea1d7300a4ce5b1
Stefan Schmidt <stefan@datenfreihafen.org>
2006-01-08 17:51:05 +0000

    - Add GPL header.
    M    trunk/reedsol.h
    M    trunk/image.c
    M    trunk/image.h
    M    trunk/reedsol.c
    
    
    git-svn-id: http://svn.datenfreihafen.org/iec16022/trunk@20 2e07f830-7309-0410-b740-c5399f01f546

--- image.c
+++ image.c
@@ -1,15 +1,22 @@
-// Image handling tools, (c) AJK 2001
-// Indended for use in applications that need to make images for web pages on the fly
-//
-// $Log: image.c,v $
-// Revision 1.6  2005/02/26 13:13:10  cvs
-// Works on 64 bit machine now
-//
-// Revision 1.5  2004/09/09 07:45:09  cvs
-// Added change history to source files
-// Added "info" type to IEC16022
-// Added exact size checking shortcodes on encoding generation for iec16022
-//
+/** 
+ *
+ * Image handling tools, (c) AJK 2001-2005
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ */ 
 
 
 #include <stdio.h>
