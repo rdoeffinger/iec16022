eaa5fd710739ebd618abd42a3ac96d6caacde562
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2022-06-11 00:26:48 +0200

    Fixes to release scripts, deal with errors slightly better.

--- release.sh
+++ release.sh
@@ -8,11 +8,14 @@ if ! grep -q "^$(date --iso-8601=date)" NEWS ; then
     echo "Expect today's date in NEWS!"
     exit 1
 fi
+rm -f "iec16022-$VER.tar.xz" "iec16022-$VER.tar.xz.asc"
 make distclean
 ./autogen.sh
 make dist
 gpg -a --detach-sign iec16022-$VER.tar.xz
 git tag -s v${VER}
+rm -rf .libs
+rm -f iec16022.signed.exe
 ./winbuild.sh
 strip .libs/iec16022.exe
 make iec16022.signed.exe
--- winbuild.sh
+++ winbuild.sh
@@ -1,3 +1,9 @@
+# Last build using popt from
+# https://repo.msys2.org/mingw/mingw32/mingw-w64-i686-popt-1.16-1-any.pkg.tar.xz
+# https://repo.msys2.org/mingw/mingw32/mingw-w64-i686-gettext-0.21-3-any.pkg.tar.zst
+# https://repo.msys2.org/mingw/mingw32/mingw-w64-i686-libiconv-1.17-1-any.pkg.tar.zst
+# add -lintl -liconv to mingw32/lib/pkgconfig/popt.pc
 make clean
-CFLAGS="-O2 -I$HOME/mxe/usr/i686-w64-mingw32/include" LDFLAGS="-L$HOME/mxe/usr/i686-w64-mingw32/lib -Wl,--nxcompat -Wl,--no-seh -Wl,--dynamicbase" ./configure  --host=i686-w64-mingw32
+make distclean
+PKG_CONFIG_PATH=mingw32/lib/pkgconfig CFLAGS="-O2 -Imingw32/include" LDFLAGS="--static -Lmingw32/lib -Wl,--nxcompat -Wl,--no-seh -Wl,--dynamicbase" ./configure  --host=i686-w64-mingw32
 make -j6
